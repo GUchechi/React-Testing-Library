b444baaa8826fe01749b809d64171585d9ec5002
GUchechi <godswilluchendu7@gmail.com>
2023-06-02 17:22:56 +0100

    test header

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-test('should render same text passed into title prop', () => {
+it('should render same text passed into title prop', () => {
   render(<Header title='My Header'/>);
   const headingElement = screen.getByText(/My Header/i);
   expect(headingElement).toBeInTheDocument();
