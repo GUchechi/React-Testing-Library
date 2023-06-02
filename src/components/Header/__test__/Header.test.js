1af5d245be8f72069e235171543efaadcedb6143
GUchechi <godswilluchendu7@gmail.com>
2023-06-02 17:56:31 +0100

    test header

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,29 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import Header from '../Header';
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
 
-it('should render same text passed into title prop', () => {
-  render(<Header title='My Header'/>);
+it("should render same text passed into title prop", () => {
+  render(<Header title="My Header" />);
   const headingElement = screen.getByText(/My Header/i);
   expect(headingElement).toBeInTheDocument();
 });
-
 
 // it('should render same text passed into title props', () => {
 //     render(<Header title='My Header'/>);
 //     const headingElement = screen.getByRole('heading');
 //     expect(headingElement).toBeInTheDocument();
 //   });
-  
 
-  it('should render text passed into title props', () => {
-    render(<Header title='My Header'/>);
-    const headingElement = screen.getByRole('heading', {name: 'My Header'});
-    expect(headingElement).toBeInTheDocument();
-  });
+it("should render text passed into title props", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByRole("heading", { name: "My Header" });
+  expect(headingElement).toBeInTheDocument();
+});
 
-  it('should render text passed into title', () => {
-    render(<Header title='My Header'/>);
-    const headingElement = screen.getByTitle('heading');
-    expect(headingElement).toBeInTheDocument();
-  });
-  
+it("should render text passed into title", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTitle("heading");
+  expect(headingElement).toBeInTheDocument();
+});
+
+// FIND BY
+
+it("should render find by text", async () => {
+  render(<Header title="My Header" />);
+  const headingElement = await screen.findByText(/My Header/i);
+  expect(headingElement).toBeInTheDocument();
+});
