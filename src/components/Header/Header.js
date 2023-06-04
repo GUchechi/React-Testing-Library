dec6c3cac0772f6a5e9ac9f5db9a34fe5cebfd07
GUchechi <godswilluchendu7@gmail.com>
2023-06-04 19:45:22 +0100

    test todoFooter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,9 @@ import "./Header.css";
 export default function Header({ title }) {
   return (
     <>
-      <h1 data-testid="testing" className="header">{title}</h1>
-      {/* <h3 title="heading" className="header">
-        Carts
-      </h3> */}
+      <h1 data-testid="testing" className="header">
+        {title}
+      </h1>
     </>
   );
 }
